801527eba271b113027c6110c20fd88672233700
hussamh10 <hussamh10@gmail.com>
2018-02-07 10:03:20 +0500

    created model

---
+++ hussam_data.py
@@ -0,0 +1,68 @@
+from cv2 import imread
+import numpy as np
+
+import numpy as np
+import cv2
+
+def getImage(i, source, main_dir):
+    name = str(i) + '.jpg'
+
+    print(main_dir + source + '\\' + name)
+
+    img = imread(main_dir + source + '\\' + name, 0)
+    img = img.reshape((img.shape[0], img.shape[1], 1))
+    img = img.astype('float32')
+    img /= 255
+    return img
+
+def getData(end, start=0):
+    uimgs = getUnetData(end, start)
+    yimgs, yauds = getYnetData(end, start)
+
+    labels = getLabels(end, start)
+
+    inputs = [yimgs, yauds, uimgs]
+
+    return inputs, labels
+
+def getUnetData(end, start=0, dir='data\\unet\\'): #data/unet/imgs/1.jpg
+    imgs = []
+    img_src = 'imgs'
+
+    for i in range(start, end):
+        image1 = getImage(i, img_src, dir)
+        image2 = getImage(i+1, img_src, dir)
+        image1 = image1.reshape((224, 224, 1))
+        image2 = image2.reshape((224, 224, 1))
+        image = np.concatenate([image1, image2], axis=2)
+        imgs.append(image)
+        
+    imgs = np.array(imgs)
+    return imgs
+
+def getLabels(end, start=0, dir='data\\ynet\\'): #data/ynet/labels
+    labels = []
+    label_src = 'labels'
+
+    for i in range(start, end):
+        i+=1
+        labels.append(getImage(i, label_src, dir))
+
+    labels = np.array(labels)
+    return labels
+
+def getYnetData(end, start=0, main_dir='data\\ynet\\'): #data/ynet/imgs/1.jpg
+    img_src = 'imgs'
+    audio_src = 'audio'
+
+    imgs = []
+    audios = []
+
+    for i in range(start, end):
+        i+=1
+        imgs.append(getImage(i, img_src, main_dir))
+        audios.append(getImage(i, audio_src, main_dir))
+        
+    imgs = np.array(imgs)
+    audios = np.array(audios)
+    return imgs, audios
---
+++ yenet.py
@@ -0,0 +1,60 @@
+import os 
+#os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+import numpy as np
+from keras.models import *
+from keras.layers import Input, merge, Conv2D, MaxPooling2D, UpSampling2D, Dropout, Cropping2D
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard
+from keras import backend as keras
+
+from unet import getUnet
+from ynet import get_unet as getYnet
+
+from hussam_data import getData
+
+def load_data():
+    imgs_train, labels_train = getData(400)
+    imgs_test, labels_test = getData(200, start = 190)
+
+    return imgs_train, labels_train, imgs_test
+
+def getYENet(r = 224, c = 224):
+
+    ynet_out, audio_inputs, ynet_inputs = getYnet(r, c)
+    unet_out, unet_inputs = getUnet(r, c)
+
+    merge01 = merge([ynet_out, unet_out], mode = 'concat', concat_axis=3)
+
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge01)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
+
+    model = Model(inputs = [ynet_inputs, audio_inputs, unet_inputs], output = conv10)
+
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+    return model
+
+def train():
+    TensorBoard(log_dir='./Graph', histogram_freq=0, 
+            write_graph=True, write_images=True)
+
+    tbCallBack = TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
+
+    print("loading data")
+
+    imgs_train, imgs_mask_train, imgs_test = load_data()
+    print("loading data done")
+
+    model = getYENet()
+    print("got yenet")
+
+    model_checkpoint = ModelCheckpoint('unet.hdf5', monitor='loss',verbose=1, save_best_only=True)
+    print('Fitting model...')
+    model.fit(imgs_train, imgs_mask_train, batch_size=4, nb_epoch=1000, verbose=1,validation_split=0.2, shuffle=True, callbacks=[model_checkpoint, tbCallBack])
+
+    print('predict test data')
+    imgs_mask_test = model.predict(imgs_test, batch_size=1, verbose=1)
+
+train()
